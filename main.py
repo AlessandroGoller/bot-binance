65ffa82a15c5e6b10489c96d97428b8c8da49fe8
Riccardo <riccardo.ricci-1@studenti.unitn.it>
2021-10-21 10:52:40 +0200

    diocane lasciami dai
    Ma dioporcaccio

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
----
 # Progetto di 
 # Riccardo Ricci e Alessandro Goller
 # https://www.linkedin.com/in/ricci-riccardo/  https://www.linkedin.com/in/alessandro-goller/
----
+
 
 
--- utility.py
+++ utility.py
@@ -1,1 +1,9 @@
-# 
+# Utilities, support functions
+
+def read_data():
+    print('Under construction')
+
+def plot_data():
+    print('Under construction')
+
+
