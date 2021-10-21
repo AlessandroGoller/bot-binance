6c15a46c02b788c12cfadc4b8d79427060d2cd63
AlessandroGoller <alessandro.goller@studenti.unitn.it>
2021-10-21 12:50:02 +0200

    funzioni aggiuntive

--- utility.py
+++ utility.py
@@ -1,6 +1,7 @@
 # Utilities, support functions
-from decouple import config
+#from decouple import config
 from binance.client import Client
+from function import *
 
 def read_data():
     print('Under construction')
@@ -8,10 +9,30 @@ def read_data():
 def plot_data():
     print('Under construction')
 
-api_key = config('binance_key')
-api_secret = config('binance_secret')
+#api_key = config('binance_key')
+#api_secret = config('binance_secret')
 
 #client = Client(api_key, api_secret)
 client = Client("", "")
 # btc_price = client.get_symbol_ticker(symbol="BTCUSDT")
 # print(btc_price)
+
+
+symbol = "ETHUSDT"
+start = "21 Oct, 2021"
+end = ""
+
+client = Client("", "")
+
+interval = Client.KLINE_INTERVAL_30MINUTE #permette di cambiare l'intervallo
+
+klines = client.get_historical_klines(symbol, interval,  start)
+save_datafile(klines,symbol, interval,  start)
+
+
+
+
+
+
+
+
