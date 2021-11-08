c361e682b45e9d756673aa6eb088feede71b9e0e
Riccardo <riccardo.ricci-1@studenti.unitn.it>
2021-11-08 16:59:27 +0100

    Further development of MACD indicator

--- macinadati.py
+++ macinadati.py
@@ -32,20 +32,38 @@ def macd(sequence,short_period,long_period):
     k_long = 2/(long_period+1)
     EMA_short=[]
     EMA_long=[]
-    EMA_short.append(sum(prices[0:short_period])/short_period)
-    EMA_long.append(sum(prices[0:long_period])/long_period)
+    EMA_short.append(sum(sequence[0:short_period])/short_period)
+    EMA_long.append(sum(sequence[0:long_period])/long_period)
     for t in range(short_period,len(sequence)):
-        EMA_short.append(prices[t]*k_short+EMA_short[t-short_period]*(1-k_short))
+        EMA_short.append(sequence[t]*k_short+EMA_short[t-short_period]*(1-k_short))
     for t in range(long_period,len(sequence)):
-        EMA_long.append(prices[t]*k_long+EMA_long[t-long_period]*(1-k_long))
+        EMA_long.append(sequence[t]*k_long+EMA_long[t-long_period]*(1-k_long))
+    
+    # Inserting zeros at the beginning of the series 
+    for i in range(long_period):
+        EMA_long.insert(0,0)
+    for i in range(short_period):
+        EMA_short.insert(0,0)
+    
+    diff = [i-j for i,j in zip(EMA_short, EMA_long)]
 
-    return EMA_long, EMA_short
+    EMA_diff = []
+    EMA_diff.append(sum(diff[0:9])/9)
+    k_diff = 2/(9+1)
+    for t in range(9,len(diff)):
+        EMA_diff.append(diff[t]*k_diff+EMA_diff[t-9]*(1-k_diff))
+    
+    for i in range(8):
+        EMA_diff.insert(0,0)
 
-emalong, emashort = macd(prices,12,26)
+    macd = [i-j for i,j in zip(diff, EMA_diff)]
+
+    return macd
+
+macd_values = macd(prices,12,26)
 
 plt.plot(prices[:1000],label='Price')
-plt.plot(emashort[:1000],label='EMA short')
-plt.plot(emalong[:1000],label='EMA long')
+plt.plot(macd_values[:1000],label='macd')
 plt.legend()
 plt.show()
 
